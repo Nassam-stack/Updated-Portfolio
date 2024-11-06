d0111148646df3b7eb8e6328e66c6f06e6b0ccd8
Nassam Jnr <nassammhabib19@gmail.com>
2024-11-06 14:34:22 +0100

    Add sound

--- Public/JavaScript Music/index.js
+++ Public/JavaScript Music/index.js
@@ -3,25 +3,25 @@ for(var i = 0; i < numberOfButtons; i++){
 document.querySelectorAll(".item")[i].addEventListener("click", function(){
    var button = this.innerHTML
    switch(button){
-    case "click on me to play black sheriff kilos millos mp3":
-        var audio1 = new Audio("./Black-Sherif-Kilos-Milos-(TrendyBeatz.com).mp3")
+    case "SOUND A":
+        var audio1 = new Audio("./sound/tom-1.mp3")
         audio1.play();
-        document.querySelector(".color1").innerHTML = "Playing now, enjoy!"
+        document.querySelector("h1").innerHTML = "Thanks for listening to the sound"
         break;
-    case "click on me to play kidi, black sheriff lomo lomo mp3":
-        var audio2 = new Audio("./KiDi-Ft-Black-Sherif-Lomo-Lomo-(TrendyBeatz.com).mp3")
+    case "SOUND B":
+        var audio2 = new Audio("./sound/tom-2.mp3")
         audio2.play();
-        document.querySelector(".color2").innerHTML = "Playing now, enjoy!"
+       
         break;
-    case "click on me to play R City ft Adam Locked away mp3":
-        var audio3 = new Audio("./R_City_ft_Adam_Levine_-_Locked_Away_(Jesusfuly.com).mp3")
+    case "SOUND C":
+        var audio3 = new Audio("./sound/tom-3.mp3")
         audio3.play();
-        document.querySelector(".color3").innerHTML = "Playing now, enjoy!"
+        
         break;
-    case "click on me to play Shain Twain You're still the one":
-        var audio4 = new Audio("./Shania_Twain_-_Youre_Still_The_One_(Jesusfuly.com).mp3")
+    case "SOUND D":
+        var audio4 = new Audio("./sound/crash.mp3")
         audio4.play();
-        document.querySelector(".color4").innerHTML = "playing now, enjoy!"
+    document.querySelector("h1").innerHTML = "Thanks for"
     default:
    }
 });
